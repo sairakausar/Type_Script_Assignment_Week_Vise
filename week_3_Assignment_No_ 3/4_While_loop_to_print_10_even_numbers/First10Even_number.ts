d22ded230ac476569f66c24537012efb1234e66c
sairakausar <sairakausar3@gmail.com>
2023-09-02 02:46:18 -0700

    Even Number

---
+++ week_3_Assignment_No_ 3/4_While_loop_to_print_10_even_numbers/First10Even_number.ts
@@ -0,0 +1,10 @@
+//Write a program that uses a while loop to print the first 10 even numbers.
+
+var even_number:number=0
+while(even_number<=10){
+    console.log("first 10 even number",even_number);
+    even_number = even_number+1
+    if(even_number%2 == 0){
+        console.log(even_number);   
+    }
+}
