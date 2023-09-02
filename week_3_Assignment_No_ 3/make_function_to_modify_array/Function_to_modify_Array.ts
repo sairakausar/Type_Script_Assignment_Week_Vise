400434f3c1d3869fc0f9b7fe7dc7f7dc3d473784
sairakausar <sairakausar3@gmail.com>
2023-09-01 20:08:55 -0700

    function and insert value in indexand modify

---
+++ week_3_Assignment_No_ 3/make_function_to_modify_array/Function_to_modify_Array.ts
@@ -0,0 +1,9 @@
+// Create a function that takes an array, an index, and a value as
+// parameters. Inside the function, use the splice method to insert the
+// value at the specified index in the array. Return the modified array.
+
+function modify_an_array(an_array:any[], index:number,value:string): void {
+     an_array.splice(index,1,value)
+    console.log(an_array);
+}
+modify_an_array([1,2,3,5,6,8,10],1,"apple")
