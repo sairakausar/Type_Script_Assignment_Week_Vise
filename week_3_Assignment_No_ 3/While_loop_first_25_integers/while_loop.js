4513acfdbf86bccb8e1fc7046488405a01b59c49
sairakausar <sairakausar3@gmail.com>
2023-09-02 02:16:29 -0700

    Print First 25 Integers

---
+++ week_3_Assignment_No_ 3/While_loop_first_25_integers/while_loop.js
@@ -0,0 +1,6 @@
+// Write a program that uses a while loop to print the first 25 integers.
+var given_number = 0;
+while (given_number <= 25) {
+    console.log(given_number);
+    given_number = given_number + 1;
+}
