ad6fb5f27466607d0ce611ed7ca75b808455244e
sairakausar <sairakausar3@gmail.com>
2023-09-02 23:56:35 -0700

    List Print in Object ( in Entity)

---
+++ Class_typeScript/class_9__Problem_solve_List_print_in_/index.js
@@ -0,0 +1,48 @@
+// // object`s of theme are same 
+var car = [{
+        company: "honda",
+        isUsedCar: true,
+        year: 2020,
+    }, {
+        company: "civic",
+        isUsedCar: false,
+        year: 2021,
+    }, {
+        company: "toyota",
+        isUsedCar: true,
+        year: 2022,
+    }, {
+        company: "kia",
+        isUsedCar: true,
+        year: 2023,
+    }, {
+        company: "BMW",
+        isUsedCar: true,
+        year: 2024,
+    }, {
+        company: "Mehran",
+        isUsedCar: true,
+        year: 2025,
+    }, {
+        company: "tiwota",
+        isUsedCar: true,
+        year: 2026,
+    }, {
+        company: "keya",
+        isUsedCar: false,
+        year: 2027,
+    }, {
+        company: "hondaa",
+        isUsedCar: true,
+        year: 2028,
+    }, {
+        company: "hondae",
+        isUsedCar: true,
+        year: 2029,
+    },
+];
+for (var index = 0; index < 1; index++) {
+    console.log(car);
+}
+console.log(car[0].year, car[1].year, car[2].year, car[3].year, car[4].year, car[5].year, car[6].year, car[7].year, car[8].year, car[9].year);
+// console.log((car[1],year[1],));
---
+++ Class_typeScript/class_9__Problem_solve_List_print_in_/index.ts
@@ -0,0 +1,64 @@
+// // object`s of theme are same 
+
+// var twwets = [{id:1, title: "sf", descrption: "yedgs"},]
+
+// typecar type = { company? : statusbar, isUsed:boolean, year: 2023}
+
+// if we no give the type or 
+// then we use ?
+//...............
+//          write a program that print all the car company
+//        and year from the list make sure car is an object
+type cartype ={ company : string,isUsedCar?: boolean, year: number}
+var car : cartype [] = [{
+    company:"honda",
+    isUsedCar: true,
+    year:2020,
+
+}, {
+    company:"civic",
+    isUsedCar:  false,
+    year:2021,
+}, {
+    company:"toyota",
+    isUsedCar: true,
+    year:2022,
+}, {
+    company:"kia",
+    isUsedCar: true,
+    year:2023,
+}, {
+    company:"BMW",
+    isUsedCar: true,
+    year:2024,
+}, {
+    company:"Mehran",
+    isUsedCar: true,
+    year:2025,
+}, {
+    company:"tiwota",
+    isUsedCar: true,
+    year:2026,
+}, {
+    company:"keya",
+    isUsedCar: false,
+    year:2027,
+}, {
+    company:"hondaa",
+    isUsedCar: true,
+    year:2028,
+}, {
+    company:"hondae",
+    isUsedCar: true,
+    year:2029,
+},
+
+]
+for (let index = 0 ; index < 1; index++){
+    console.log(car);  
+}
+console.log(car[0].year,car[1].year,car[2].year,car[3].year,car[4].year,car[5].year,car[6].year,car[7].year,car[8].year,car[9].year);
+
+
+
+// console.log((car[1],year[1],));
