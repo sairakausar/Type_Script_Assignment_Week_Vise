b7421bf78c193495e36bc2515236999169efe9d4
sairakausar <sairakausar3@gmail.com>
2023-09-07 22:14:58 -0700

    Print number 8 in 8 lines

--- 4_September_2023___45_Assignment_TypeScript/2_Assignment/index.ts
+++ 4_September_2023___45_Assignment_TypeScript/2_Assignment/index.ts
@@ -1,9 +1,12 @@
-//    Personal Message: Store a person’s name in a variable, and print a message 
+//    Personal Message: Store a person’s name_ in a variable, and print a message 
 //    to that person. Your message should be simple, such as, “Hello Eric, would
 //     you like to learn some Python today?”
 
 var name_ : string = "Hello Eric, would you like to learn some python today?";
             console.log(name_);
+            var name_w : string = "The End"
+            
+                         
             
 
 
